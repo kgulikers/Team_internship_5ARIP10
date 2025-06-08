c12b0fa9f2779859c1d7a4773672cd9f78bf7a5f
JoostLeliveld <75120891+JoostLeliveld@users.noreply.github.com>
2025-06-08 22:26:25 +0200

    Update actions.py

--- source/avulab_tasks/avulab_tasks/common/actions.py
+++ source/avulab_tasks/avulab_tasks/common/actions.py
@@ -1,73 +1,16 @@
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This file is part of the Avular Origin One project.
+#
+# Copyright (c) 2025–2025, Avulab Project Developers (5ARIP10).
+# All rights reserved.
+#
+# This file was created specifically for the Origin One platform.
+
+
 from isaaclab.utils import configclass
-
-from avulab.envs.mdp import RCCar4WDActionCfg, RCCarRWDActionCfg
-
-@configclass
-class MushrRWDActionCfg:
-
-    throttle_steer = RCCarRWDActionCfg(
-        wheel_joint_names=[
-            "back_left_wheel_throttle",
-            "back_right_wheel_throttle",
-        ],
-        steering_joint_names=[
-            "front_left_wheel_steer",
-            "front_right_wheel_steer",
-        ],
-        base_length=0.325,
-        base_width=0.2,
-        wheel_radius=0.05,
-        scale=(3.0, 0.488),
-        no_reverse=True,
-        bounding_strategy="clip",
-        asset_name="robot",
-    )
-
-
-@configclass
-class Mushr4WDActionCfg:
-
-    throttle_steer = RCCar4WDActionCfg(
-        wheel_joint_names=[
-            "back_left_wheel_throttle",
-            "back_right_wheel_throttle",
-            "front_left_wheel_throttle",
-            "front_right_wheel_throttle",
-        ],
-        steering_joint_names=[
-            "front_left_wheel_steer",
-            "front_right_wheel_steer",
-        ],
-        base_length=0.325,
-        base_width=0.2,
-        wheel_radius=0.05,
-        scale=(3.0, 0.488),
-        no_reverse=True,
-        bounding_strategy="clip",
-        asset_name="robot",
-    )
-
-
 from avulab.envs.mdp import DifferentialDriveActionCfg
 
-@configclass
-class SkidSteerActionCfg:
-    throttle = DifferentialDriveActionCfg(
-        left_wheel_joint_names=[
-            "front_left_wheel_throttle",
-            "back_left_wheel_throttle",
-        ],
-        right_wheel_joint_names=[
-            "front_right_wheel_throttle",
-            "back_right_wheel_throttle",
-        ],
-        wheel_radius=0.12,
-        wheel_base=0.5,   
-        scale=(1.0, 1.0),         
-        bounding_strategy="clip",
-        no_reverse=False,
-        asset_name="robot",
-    )
 
 @configclass
 class OriginActionCfg:
